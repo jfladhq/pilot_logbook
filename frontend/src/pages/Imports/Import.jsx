8bb01ae4e97c0a618579d29d922630dc19fd6a24
jflad17 <56899029+jflad17@users.noreply.github.com>
2025-04-12 02:15:33 -0500

    Completed all frontend fixes from reverted Dev branch that didin't merge in.

---
+++ frontend/src/pages/Imports/Import.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import UploadFiles from '@components/UploadFiles/UploadFiles';
+
+/**
+ *
+ * @return {Component} Import Component
+ */
+function Import() {
+  return (
+    <>
+      <UploadFiles title='File Import'/>
+    </>
+  );
+}
+
+export default Import;
