4b7d75206d9d7055985d9d896ac45155c76bc70c
jflad17 <56899029+jflad17@users.noreply.github.com>
2025-04-25 01:42:21 -0500

    Cleanup and fixed misc issues on backend

--- backend/api/routes/flight/airport.py
+++ backend/api/routes/flight/airport.py
@@ -1,11 +1,11 @@
 from dependencies import get_db
 from fastapi_crudrouter import SQLAlchemyCRUDRouter as CRUDRouter
-import schemas as schemas
 from src.airport import models
 from src.airport import schemas
 router = CRUDRouter(
     schema=schemas.Airport,
     create_schema=schemas.Airport,
+    update_schema=schemas.Airport,
     db_model=models.Airport,
     db=get_db,
     prefix="/airport",
--- backend/schemas/user.py
+++ backend/schemas/user.py
@@ -1,32 +1,30 @@
-# from .base import Base
+from .base import Base
 
 
-# class User(Base):
-#     id: int
-#     admin: bool
-#     username: str
-#     email: str
-#     firstName: str
-#     lastName: str
-#     resetPassword: bool
-#     loginAttempts: int
-#     maxLoginAttempts: int
+class User(Base):
+    id: int
+    admin: bool
+    username: str
+    firstName: str
+    lastName: str
+    resetPassword: bool
+    loginAttempts: int
+    maxLoginAttempts: int
 
 
-# class Token(Base):
-#     access_token: str
-#     token_type: str
-#     user: User
+class Token(Base):
+    access_token: str
+    token_type: str
+    user: User
 
 
-# class TokenData(Base):
-#     username: str | None = None
-#     scopes: list[str] = []
+class TokenData(Base):
+    username: str | None = None
+    scopes: list[str] = []
 
 
-# class NewUser(Base):
-#     username: str
-#     firstName: str
-#     lastName: str
-#     password: str
-#     email: str
+class NewUser(Base):
+    username: str
+    firstName: str
+    lastName: str
+    password: str
